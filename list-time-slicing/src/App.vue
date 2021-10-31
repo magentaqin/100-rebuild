73185a499a80e18bacc9b1d22f8de446a9255c88
qinmu <magenta2127@gmail.com>
2021-10-31 16:35:30 +0800

    feat: 长列表优化-时间分片-一次性渲染10万条数据

---
+++ list-time-slicing/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <large-list />
+  </div>
+</template>
+
+<script>
+import LargeList from './components/LargeList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LargeList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ list-time-slicing/src/components/LargeList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h3>一次性渲染10万条数据</h3>
+    <ul id="container">
+      <li v-for="(item, index) in list" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: []
+    }
+  },
+  mounted() {
+    for (let i = 0; i < 100000; i++) {
+      this.list.push(i)
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  color: #42b983;
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+
+</style>
